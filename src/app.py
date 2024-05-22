7fab14f99664254f40fce7685b44f65c37a2753f
Lucian Bellini <lucianbellini1@gmail.com>
2024-05-21 21:56:53 -0300

    21-05

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #url
-url = os.getenv('URL')
+url = "https://docs.google.com/spreadsheets/d/1Y6R653wcsuvHOwTntZ6tr3EjtHksz5hhmg7YgKaatFo/edit?usp=sharing"
 
 # Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
